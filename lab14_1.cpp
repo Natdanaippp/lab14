69b7bf3c914ca3bdaedfec5949b8d1334d366ac7
ppp <natdanai_kamthorn@cmu.ac.th>
2024-01-05 16:59:26 +0700

    ppp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,46 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int a=1;a<N;a++)
+	{
+		cout << a<<"  ";
+		for(int i=a-1;i>=0;i--)	
+		{	
+			int r=0;
+			int f;
+			int asd = d[a];
+			if(d[a]>=d[i])
+			{
+				if(i==0)
+				{
+					for(int j=a;j>=0;j--)
+					{
+						d[j]=d[j-1];
+					}
+					d[0]=asd;
+					continue;
+				}
+				f=i;
+				r+=1;
+				continue;
+			}
+			else
+			{
+				if(r!=0)
+				{
+					for(int i=a;i>=f;i--)	
+					{
+						d[i]=d[i-1];
+					}
+					d[f]=asd;
+				}
+				
+			}
+		}
+		for(int i = 0; i < 10; i++) cout << d[i] << " ";
+		cout << "\n";
+	}
+	
 }
 
 int main(){
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,45 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool a[][N])
+	{
+		for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<N;j++)
+			{
+				cout<<a[i][j]<<" ";
+			}
+			cout<<"\n";
+		}
+	}
+void inputMatrix(double a[][N])
+	{
+		for(int i=0;i<5;i++)
+		{
+			cout<<"Row "<<i+1<<": ";
+			for(int j=0;j<5;j++)
+			{
+				cin>>a[i][j];
+			}
+		}
+	}
+void findLocalMax(const double a[][N], bool s[][N])
+{
+    for(int i=0;i<5;i++)
+    		{
+    			for(int j=0;j<5;j++)
+    			{
+    			    s[i][j]=0;
+    			}
+    		}
+    for(int i=1;i<4;i++)
+		{
+			for(int j=1;j<4;j++)
+			{
+				if(a[i][j]>=a[i-1][j]&&a[i][j]>=a[i+1][j]&&a[i][j]>=a[i][j-1]&&a[i][j]>=a[i][j+1])
+				{
+				    s[i][j]=1;
+				}
+			}
+		}
+}
